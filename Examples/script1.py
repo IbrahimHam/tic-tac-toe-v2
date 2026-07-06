49183018794d84e63dd155948327fbc3139f3836
Ibrahim <dev.ibrahimhammoud@gmail.com>
2026-07-06 11:00:13 +0200

    conflect

--- Examples/script1.py
+++ Examples/script1.py
@@ -1,7 +1,10 @@
 print ("Always executed")
 
 def calculate_sum(a, b):
-    return a + b
+    if a < 0 :
+        return a + b
+    else:
+        return a + b + 1
  
 if __name__ == "__main__":
     print ("Executed when invoked directly")
